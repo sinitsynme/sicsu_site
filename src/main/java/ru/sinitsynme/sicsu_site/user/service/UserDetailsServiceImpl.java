f8b625a0889e0ab81848a791176f3643b85ddb8a
sinitsynme <72615475+sinitsynme@users.noreply.github.com>
2021-03-19 22:25:00 +0400

    Add methods for getting, saving and deleting users

--- src/main/java/ru/sinitsynme/sicsu_site/user/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/sinitsynme/sicsu_site/user/service/UserDetailsServiceImpl.java
@@ -2,13 +2,13 @@ package ru.sinitsynme.sicsu_site.user.service;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ru.sinitsynme.sicsu_site.UserData;
 import ru.sinitsynme.sicsu_site.studentData.service.StudentDataService;
 import ru.sinitsynme.sicsu_site.teacherData.service.TeacherDataService;
 import ru.sinitsynme.sicsu_site.user.entity.User;
@@ -19,6 +19,7 @@ import ru.sinitsynme.sicsu_site.user.repository.UserRepository;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
   private final UserRepository userRepository;
+  //is there a point in services there?
   private final TeacherDataService teacherDataService;
   private final StudentDataService studentDataService;
 
@@ -36,32 +37,40 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     return userRepository.findByUsername(s);
   }
 
-  public User getCurrentUser(){
+  public User getCurrentUser() {
     return (User) loadUserByUsername(getCurrentUserName());
   }
 
-
-  public List<User> getUserList(){
-    return userRepository.findAll();
+  public Optional<User> getUserById(Long id) {
+    return userRepository.findById(id);
   }
 
-  public String getCurrentUserName(){
+  public List<User> getUserList() {
+    return userRepository.findAll(Sort.by("id"));
+  }
+
+  public void saveUser(User user) {
+    userRepository.save(user);
+  }
+
+  public String getCurrentUserName() {
     return SecurityContextHolder.getContext().getAuthentication().getName();
   }
 
 
-  public Optional<?> getCurrentUserData(){
+  public Optional<UserData> getCurrentUserData() {
     User currentUser = getCurrentUser();
-    if(currentUser.isStudent()){
-      return studentDataService.getStudentDataById(currentUser.getId());
-    }
-    else if(currentUser.isTeacher()){ //edit repository
-      return teacherDataService.getTeacherById(currentUser.getId());
+    if (currentUser.isStudent()) {
+      return Optional.ofNullable(currentUser.getStudentData());
+    } else if (currentUser.isTeacher()) {
+      return Optional.ofNullable(currentUser.getTeacherData());
     }
     return Optional.empty();
   }
 
-
+  public void deleteUser(User user) {
+    userRepository.delete(user);
+  }
 
 
 }
