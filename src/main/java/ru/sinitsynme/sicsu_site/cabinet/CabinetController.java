6142ff02471da57d37ac611ca01dc1add82995bb
sinitsynme <72615475+sinitsynme@users.noreply.github.com>
2021-03-19 22:01:27 +0400

    Spread cabinets for student and teacher

--- src/main/java/ru/sinitsynme/sicsu_site/cabinet/CabinetController.java
+++ src/main/java/ru/sinitsynme/sicsu_site/cabinet/CabinetController.java
@@ -1,10 +1,14 @@
 package ru.sinitsynme.sicsu_site.cabinet;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.sinitsynme.sicsu_site.UserData;
+import ru.sinitsynme.sicsu_site.studentData.entity.StudentData;
+import ru.sinitsynme.sicsu_site.teacherData.entity.TeacherData;
 import ru.sinitsynme.sicsu_site.user.service.UserDetailsServiceImpl;
 
 @Controller
@@ -21,13 +25,19 @@ public class CabinetController {
 
   @GetMapping
   public String getCabinetPage(Model model) {
-    if (userDetailsService.getCurrentUserData().isPresent()) {
-      model.addAttribute("userData", userDetailsService.getCurrentUserData().get());
-      return "cabinet";
+    Optional<UserData> optionalUserData = userDetailsService.getCurrentUserData();
+    if (optionalUserData.isPresent()) {
+      UserData userData = optionalUserData.get();
+      model.addAttribute("userData", userData);
+      if(userData instanceof StudentData){
+        return "cabinet/student";
+      }
+      else if(userData instanceof TeacherData){
+        return "cabinet/teacher";
+      }
     }
     model.addAttribute("error", "Missing data");
-    return "cabinet";
+    return "errorPage";
   }
-
 
 }
