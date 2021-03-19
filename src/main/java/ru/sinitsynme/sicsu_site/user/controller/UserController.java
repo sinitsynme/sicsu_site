e27e3bfd1c40dfe046e05981912ffe16cfef1063
sinitsynme <72615475+sinitsynme@users.noreply.github.com>
2021-03-19 22:19:17 +0400

    Add patch and delete for users

--- src/main/java/ru/sinitsynme/sicsu_site/user/controller/UserController.java
+++ src/main/java/ru/sinitsynme/sicsu_site/user/controller/UserController.java
@@ -1,11 +1,16 @@
 package ru.sinitsynme.sicsu_site.user.controller;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.sinitsynme.sicsu_site.user.entity.User;
 import ru.sinitsynme.sicsu_site.user.service.UserDetailsServiceImpl;
 
 @Controller
@@ -23,6 +28,71 @@ public class UserController {
   @GetMapping
   public String listAllUsers(Model model) {
     model.addAttribute("users", service.getUserList());
-    return "userlist";
+    return "userList";
   }
+
+  @GetMapping("/{id}")
+  public String getUserPage(Model model, @PathVariable Long id){
+    Optional<User> optionalUser = service.getUserById(id);
+    if(optionalUser.isPresent()){
+      User user = optionalUser.get();
+      if (user.getStudentData() != null){
+        model.addAttribute("userData", user.getStudentData());
+        return "cabinet/student";
+      }
+      else if(user.getTeacherData() != null){
+        model.addAttribute("userData", user.getTeacherData());
+        return "cabinet/teacher";
+      }
+      else {
+        model.addAttribute("error", "Data is unavailable");
+        return "errorPage";
+      }
+    }
+    else{
+      model.addAttribute("error", "Cannot find user with such id");
+      return "errorPage";
+    }
+  }
+
+  @DeleteMapping("/delete/{id}")
+  public String deleteUser(Model model, @PathVariable Long id){
+    Optional<User> userOptional = service.getUserById(id);
+    if(userOptional.isPresent()){
+      User user = userOptional.get();
+      service.deleteUser(user);
+      return "redirect:/users";
+    }
+    else{
+      model.addAttribute("error", "Cannot find user with such id");
+      return "errorPage";
+    }
+  }
+
+  @GetMapping("/{id}/edit")
+  public String getEditForm(Model model, @PathVariable Long id){
+    Optional<User> userOptional = service.getUserById(id);
+    if(userOptional.isPresent()){
+      User user = userOptional.get();
+      model.addAttribute("userRegistering", user);
+      if (user.getStudentData() != null){
+        model.addAttribute("userData", user.getStudentData());
+        return "authReg/studentReg";
+      }
+      else if(user.getTeacherData() != null){
+        model.addAttribute("userData", user.getTeacherData());
+        return "authReg/teacherReg";
+      }
+      else {
+        model.addAttribute("error", "Data is unavailable");
+        return "errorPage";
+      }
+    }
+    else{
+      model.addAttribute("error", "Cannot find user with such id");
+      return "errorPage";
+    }
+  }
+
+
 }
