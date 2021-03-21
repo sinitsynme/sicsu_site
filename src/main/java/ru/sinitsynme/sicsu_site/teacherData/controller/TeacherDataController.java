3571a8547fdd6164cfbd87e1cc02e832d50dfe03
sinitsynme <72615475+sinitsynme@users.noreply.github.com>
2021-03-21 21:04:03 +0400

    Add teacher registration

--- src/main/java/ru/sinitsynme/sicsu_site/teacherData/controller/TeacherDataController.java
+++ src/main/java/ru/sinitsynme/sicsu_site/teacherData/controller/TeacherDataController.java
@@ -1,8 +1,34 @@
 package ru.sinitsynme.sicsu_site.teacherData.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.sinitsynme.sicsu_site.teacherData.entity.TeacherData;
+import ru.sinitsynme.sicsu_site.teacherData.service.TeacherDataService;
 
 @Controller
+@RequestMapping("/teachers")
 public class TeacherDataController {
+
+  private final TeacherDataService teacherDataService;
+
+  @Autowired
+  public TeacherDataController(
+      TeacherDataService teacherDataService) {
+    this.teacherDataService = teacherDataService;
+  }
+
+  @PostMapping("/new")
+  public String registerTeacher(TeacherData teacherData, Long userId, Model model){
+    if(teacherDataService.registerTeacher(teacherData, userId)){
+      return "redirect:/users";
+    }
+    model.addAttribute("error", "Something went wrong");
+    return "errorPage";
+  }
+
+
 
 }
--- src/main/java/ru/sinitsynme/sicsu_site/teacherData/service/TeacherDataService.java
+++ src/main/java/ru/sinitsynme/sicsu_site/teacherData/service/TeacherDataService.java
@@ -4,19 +4,41 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import ru.sinitsynme.sicsu_site.teacherData.entity.TeacherData;
 import ru.sinitsynme.sicsu_site.teacherData.repository.TeacherDataRepository;
+import ru.sinitsynme.sicsu_site.user.entity.User;
+import ru.sinitsynme.sicsu_site.user.service.UserService;
 
 
 @Service
 public class TeacherDataService {
 
   private final TeacherDataRepository teacherDataRepository;
+  private final UserService userService;
 
   public TeacherDataService(
-      TeacherDataRepository teacherDataRepository) {
+      TeacherDataRepository teacherDataRepository,
+      UserService userService) {
     this.teacherDataRepository = teacherDataRepository;
+    this.userService = userService;
   }
 
-  public Optional<TeacherData> getTeacherById(Long id){
+  public Optional<TeacherData> getTeacherById(Long id) {
     return teacherDataRepository.findById(id);
   }
+
+  public boolean registerTeacher(TeacherData teacherData, Long userId) {
+    Optional<User> userOptional = userService.getUserById(userId);
+    if(userOptional.isPresent()){
+      User user = userOptional.get();
+      user.setActive(true);
+      user.setTeacherData(teacherData);
+      teacherData.setUser(user);
+
+      teacherDataRepository.save(teacherData);
+      userService.saveUser(user);
+      return true;
+    }
+    return false;
+  }
+
+
 }
