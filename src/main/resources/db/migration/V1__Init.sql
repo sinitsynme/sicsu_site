0b9767657e5ba6d473359ac7cf4a63ab7fb5eb23
sinitsynme <72615475+sinitsynme@users.noreply.github.com>
2021-08-04 11:59:14 +0400

    Fix bad migration file

--- src/main/resources/db/migration/V1__Init.sql
+++ src/main/resources/db/migration/V1__Init.sql
@@ -1,93 +1,129 @@
-create sequence seq_group start 1 increment 1;;
-create sequence seq_student start 1 increment 1;
-create sequence seq_teacher start 1 increment 1;
-create sequence seq_user start 1 increment 1;
-
-create table groups
-(
-    id            int8 not null,
-    group_full_id varchar(255),
-    info          varchar(255),
-    primary key (id)
+create table department (
+                                   id uuid not null,
+                                   name varchar(255),
+                                   faculty_id uuid,
+                                   primary key (id)
 );
 
-create table student_data
-(
-    id            int8 not null,
-    birth_date    date,
-    course_number int2 not null,
-    email         varchar(255),
-    first_name    varchar(255),
-    last_name     varchar(255),
-    primary key (id)
+
+create table faculty (
+                                id uuid not null,
+                                name varchar(255),
+                                primary key (id)
 );
 
-create table students_groups
-(
-    group_id        int8,
-    student_data_id int8 not null,
-    primary key (student_data_id)
+create table groups (
+                        id uuid not null,
+                        group_full_id varchar(255),
+                        program_id uuid,
+                        primary key (id)
 );
 
-create table teacher_data
-(
-    id         int8 not null,
-    email      varchar(255),
-    first_name varchar(255),
-    last_name  varchar(255),
-    primary key (id)
+
+create table personal_data (
+                                      id uuid not null,
+                                      birth_date date,
+                                      first_name varchar(255),
+                                      last_name varchar(255),
+                                      patronymic varchar(255),
+                                      primary key (id)
 );
 
-create table teacher_data_disciplines
-(
-    teacher_data_id int8 not null,
-    disciplines     varchar(255)
+create table program (
+                                id uuid not null,
+                                name varchar(255),
+                                type varchar(255),
+                                faculty_id uuid,
+                                primary key (id)
 );
 
-create table teachers_groups
-(
-    teacher_id int8 not null,
-    groups_id  int8 not null,
-    primary key (teacher_id, groups_id)
+create table student (
+                         id uuid not null,
+                         info varchar(255),
+                         group_id uuid,
+                         personal_data_id uuid,
+                         primary key (id)
 );
 
-create table user_role
-(
-    user_id int8 not null,
-    roles   varchar(255)
+create table teacher (
+                                id uuid not null,
+                                achievements varchar(255),
+                                info varchar(255),
+                                department_id uuid,
+                                personal_data_id uuid,
+                                primary key (id)
 );
 
-create table usr
-(
-    id              int8 not null,
-    is_active       boolean,
-    password        varchar(255),
-    username        varchar(255),
-    student_data_id int8,
-    teacher_data_id int8,
-    primary key (id)
+create table user_role (
+                           user_id uuid not null,
+                           roles varchar(255)
 );
 
-alter table if exists students_groups
-    add constraint students_groups_group_fk foreign key (group_id) references groups;
+create table usr (
+                     id uuid not null,
+                     credentials_non_expired boolean,
+                     email varchar(255),
+                     is_active boolean,
+                     password varchar(255),
+                     username varchar(255),
+                     personal_data_id uuid,
+                     student_id uuid,
+                     teacher_id uuid,
+                     primary key (id)
+);
 
-alter table if exists students_groups
-    add constraint students_groups_studdata_fk1 foreign key (student_data_id) references student_data on delete cascade;
+alter table if exists department
+    add constraint department_faculty_fk
+        foreign key (faculty_id)
+            references faculty;
 
-alter table if exists teacher_data_disciplines
-    add constraint teacher_data_disc_fk foreign key (teacher_data_id) references teacher_data;
 
-alter table if exists teachers_groups
-    add constraint teachers_groups_group_fk foreign key (groups_id) references groups;
+alter table if exists groups
+    add constraint groups_program_fk
+        foreign key (program_id)
+            references program;
 
-alter table if exists teachers_groups
-    add constraint teachers_groups_teachdata_fk1 foreign key (teacher_id) references teacher_data on delete cascade;
+alter table if exists program
+    add constraint program_faculty_fk
+        foreign key (faculty_id)
+            references faculty;
+
+alter table if exists student
+    add constraint student_group_fk
+        foreign key (group_id)
+            references groups;
+
+alter table if exists student
+    add constraint student_personal_data_fk
+        foreign key (personal_data_id)
+            references personal_data;
+
+alter table if exists teacher
+    add constraint teacher_department_fk
+        foreign key (department_id)
+            references department;
+
+alter table if exists teacher
+    add constraint teacher_personal_data_fk
+        foreign key (personal_data_id)
+            references personal_data;
 
 alter table if exists user_role
-    add constraint user_role_user_fk foreign key (user_id) references usr;
+    add constraint user_fk
+        foreign key (user_id)
+            references usr;
 
 alter table if exists usr
-    add constraint usr_studdata_fk foreign key (student_data_id) references student_data;
+    add constraint user_personal_data_fk
+        foreign key (personal_data_id)
+            references personal_data;
 
 alter table if exists usr
-    add constraint usr_teachdata_fk foreign key (teacher_data_id) references teacher_data;
+    add constraint usr_student_fk
+        foreign key (student_id)
+            references student;
+
+alter table if exists usr
+    add constraint usr_teacher_fk
+        foreign key (teacher_id)
+            references teacher;
